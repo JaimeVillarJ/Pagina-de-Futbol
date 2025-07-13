52fad00080d73039241ce18440f539f0fd0bfa39
JaimeVillarJ <jaimevvvjjj@gmail.com>
2025-07-12 19:51:50 -0500

    segundo cambio

--- src/components/Calendario.tsx
+++ src/components/Calendario.tsx
@@ -7,6 +7,7 @@ export const Calendario = () => {
         <div className="contenedor-calendario">
             <h1 className="titulo-general">Calendario de Partidos</h1>
             <h1>Hola mundo</h1>
+            <h1>Adios mundo</h1>
         </div>
     </>
   )
